d823f3c4cc3e93d12f049752964484f5b58ba560
Adrsh005 <aadarshmani2005@gmail.com>
2025-08-28 13:12:49 +0530

    Update 5. 3-Bit LFSR .v

--- Circuits/Sequential Circuits/Shift resistors/5. 3-Bit LFSR .v
+++ Circuits/Sequential Circuits/Shift resistors/5. 3-Bit LFSR .v
@@ -1,25 +1,15 @@
 module top_module (
-	input [2:0] SW,      
-	input [1:0] KEY,     
-	output [2:0] LEDR
-  );
-  
-  wire l;
-  wire clk;
-  wire [2:0] R;
-  reg [2:0] Q;
-  
-  assign R = SW;
-  assign clk = KEY[0];
-  assign l = KEY[1];
-        
-  always @(posedge clk) begin
-    if(l)
-          Q <= R;
-      else
-          Q <= {Q[2]^Q[1], Q[0], Q[2]};
-  end
-    
-  assign LEDR = Q;
-
+	input [2:0] SW,      // R
+	input [1:0] KEY,     // L and clk
+	output [2:0] LEDR);  // Q
+    wire L,clk;
+    assign {L,clk}=KEY;
+    reg [2:0] q,;    
+    always @ (posedge clk) begin
+        if (L)
+            q<=SW;
+        else
+            q<={q[2]^q[1],q[0],q[2]};
+    end
+    assign LEDR=q;
 endmodule
