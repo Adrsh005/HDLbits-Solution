4c8ae5944c197278c98f1b9b190c116867ebc659
Adrsh005 <aadarshmani2005@gmail.com>
2025-09-02 10:52:15 +0530

    Create 4. Combinational Circuit 4 .v

---
+++ Verification: Reading Simulation/Build Circuit from Simulation Waveform/4. Combinational Circuit 4 .v
@@ -0,0 +1,11 @@
+// This logic just worked, I haven't simplified the truth table
+module top_module (
+    input a,
+    input b,
+    input c,
+    input d,
+    output q );//
+
+    assign q = c | b ; // Fix me
+
+endmodule
